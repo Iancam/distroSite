419dfa142e28ae7bcb16d19cca6da00209d8f595
Ian Campbell <ian.campbell.92@gmail.com>
2016-05-02 21:22:33 -0600

    flushed cache, things are working

--- app/assets/javascripts/components/StateOptions.js.jsx
+++ app/assets/javascripts/components/StateOptions.js.jsx
@@ -1,3 +1,4 @@
+const React = require('react')
 const stateOptions = [["","N/A"],["AL","Alabama"],["AK","Alaska"],["AZ","Arizona"],["AR","Arkansas"],["CA","California"],["CO","Colorado"],["CT","Connecticut"],["DE","Delaware"],["FL","Florida"],["GA","Georgia"],["HI","Hawaii"],["ID","Idaho"],
                           ["IL","Illinois"],["IN","Indiana"],["IA","Iowa"],["KS","Kansas"],["KY","Kentucky"],["LA","Louisiana"],["ME","Maine"],["MD","Maryland"],["MA","Massachusetts"],["VA","Virginia"],["MI","Michigan"],["WA","Washington"],
                           ["MN","Minnesota"],["WV","West Virginia"],["MS","Mississippi"],["WI","Wisconsin"],["MO","Missouri"],["WY","Wyoming"],["MT","Montana"],["NE","Nebraska"],["NV","Nevada"],["NH","New Hampshire"],["NJ","New Jersey"],["NM","New Mexico"],
